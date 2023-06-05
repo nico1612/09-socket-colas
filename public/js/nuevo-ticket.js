c09d8ba7845d36f3525e56103db0f9364b19ad16
nico1612 <nicogonzalezraggi123@hotmail.com>
2023-06-05 14:54:26 -0300

    fin socket tickets

--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,31 @@
 
+const lblNuevoTicket = document.querySelector('#lblNuevoTicket')
+const btnCrear = document.querySelector('button')
 
-console.log('Nuevo Ticket HTML');
+const socket = io();
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+
+    btnCrear.disabled = false
+
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+
+    btnCrear.disabled = true
+
+});
+
+socket.on('ultimo-ticket',(ultimo)=>{
+    lblNuevoTicket.innerText='Ticket' + ultimo
+})
+
+btnCrear.addEventListener( 'click', () => {
+
+    socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+        lblNuevoTicket.innerText=ticket
+    });
+
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -6,12 +6,44 @@ export const socketController = (socket) => {
 
     socket.on('disconnect', () => {});
 
-    socket.on('enviar-mensaje', ( payload, callback ) => {
+    socket.emit( 'ultimo-ticket', ticketControl.ultimo )
+    socket.emit( 'estado-actual', ticketControl.ultimos4 );
+    socket.emit('estados-pendientes',ticketControl.tickets.length)
 
-        const id = 123456789;
-        callback( id );
+    socket.on('siguiente-ticket', ( payload, callback ) => {
+        
+        const siguiente = ticketControl.siguiente();
+        callback( siguiente );
+        socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length);
 
-        socket.broadcast.emit('enviar-mensaje', payload );
+    });
 
+    socket.on('atender-ticket',({escritorio},callback)=>{
+        
+        if(!escritorio){
+            return callback({
+                ok:false,
+                msg:'El escritorio es obligatorio'
+            })
+        }
+
+        const ticket = ticketControl.atenderTickets(escritorio)
+
+        socket.broadcast.emit( 'estado-actual', ticketControl.ultimos4 );
+        socket.emit( 'tickets-pendientes', ticketControl.tickets.length);
+        socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length);
+
+        if(!ticket){
+            return callback({
+                ok:false,
+                msg:'ya no hay tickets pendientes'
+            })
+        }
+        else{
+            return callback({
+                ok:true,
+                ticket
+            })
+        }
     })
 }
